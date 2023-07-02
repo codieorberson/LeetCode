0a3248138c789b417d656573fc23142dccf653d0
codieorberson <codieorberson@gmail.com>
2023-07-02 18:33:01 -0500

    Add LoggerRateLimiterProblem

---
+++ LeetCode/EasyProblems/LoggerRateLimiterProblem.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class LoggerRateLimiterProblem
+    {
+        Dictionary<string, int> loggerDictionary;
+        public LoggerRateLimiterProblem()
+        {
+            loggerDictionary = new Dictionary<string, int>();
+        }
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if(loggerDictionary.ContainsKey(message) && timestamp < loggerDictionary[message]) {
+                return false;
+            }
+            loggerDictionary[message] = timestamp + 10;
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/LoggerRateLimiterProblem.Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.EasyProblems;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class LoggerRateLimiterProblemTest
+    {
+        LoggerRateLimiterProblem loggerRateLimiterProblem;
+
+        [TestInitialize]
+        public  void Setup()
+        {
+            loggerRateLimiterProblem = new LoggerRateLimiterProblem();
+        }
+
+       
+        [TestMethod]
+        public void ShouldPrintMessage()
+        {
+            Assert.AreEqual(true, loggerRateLimiterProblem.ShouldPrintMessage(1, "foo"));
+            Assert.AreEqual(true, loggerRateLimiterProblem.ShouldPrintMessage(2, "bar"));
+            Assert.AreEqual(false, loggerRateLimiterProblem.ShouldPrintMessage(3, "foo"));
+            Assert.AreEqual(false, loggerRateLimiterProblem.ShouldPrintMessage(8, "bar"));
+            Assert.AreEqual(false, loggerRateLimiterProblem.ShouldPrintMessage(10, "foo"));
+            Assert.AreEqual(true, loggerRateLimiterProblem.ShouldPrintMessage(11, "foo"));
+        }
+    }
+}
