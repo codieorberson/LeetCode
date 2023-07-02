eb6248eab608c3b51faf5ab540f4ea95373b9609
codieorberson <codieorberson@gmail.com>
2023-07-02 17:42:47 -0500

    Add Valid Parentheses Q

---
+++ LeetCode/EasyProblems/ValidParenthesesProblem.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class ValidParenthesesProblem
+    {
+
+        public ValidParenthesesProblem() { }
+
+        public bool ValidParentheses(string symbols)
+        {
+
+            Dictionary<char, char> validSymbols = new Dictionary<char, char>()
+            {
+                {')', '('},
+                { ']', '[' },
+                {'}', '{' },
+            };
+
+            Stack<char> allSymbols = new Stack<char>();
+
+            foreach (char symbol in symbols) {
+                if (!validSymbols.ContainsKey(symbol))
+                {
+                    allSymbols.Push(symbol);
+                }
+                else if (allSymbols.Count == 0 || allSymbols.Pop() != validSymbols[symbol])
+                {
+                    return false;
+                }
+            }
+
+            return (allSymbols.Count == 0);
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/ValidParenthesesProblem.Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.EasyProblems;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class ValidParenthesesProblemTest
+    {
+
+        ValidParenthesesProblem validParenthesesProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            validParenthesesProblem = new ValidParenthesesProblem();
+        }
+
+        [TestMethod]
+        [DataRow("()", true)]
+        [DataRow("()[]{}", true)]
+        [DataRow("(]", false)]
+        [DataRow("(]", false)]
+        [DataRow("(()))", false)]
+        [DataRow("([{}]))", false)]
+        public void ValidParentheses(string value, bool expected) 
+        {
+            bool actual = validParenthesesProblem.ValidParentheses(value);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
