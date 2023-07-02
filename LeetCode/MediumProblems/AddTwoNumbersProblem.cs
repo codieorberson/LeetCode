64dbd6a55e7d497830e81e123bf4fc953e7f65d4
codieorberson <codieorberson@gmail.com>
2023-07-01 23:43:47 -0500

    Add MediumProblem AddTwoNumbers

---
+++ LeetCode/MediumProblems/AddTwoNumbersProblem.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode.MediumProblems
+{
+    public class AddTwoNumbersProblem
+    {
+        public AddTwoNumbersProblem() { }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0) 
+        {
+            if (l1 == null && l2 == null && carry == 0) return null;
+
+            int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+
+            return new ListNode() { val = sum, next = AddTwoNumbers(l1?.next, l2?.next, carry)};
+        }
+
+        public ListNode AddTwoNumbers2(ListNode l1, ListNode l2)
+        {
+            ListNode parentNode = new ListNode();
+            ListNode currentNode = parentNode;
+
+            int carry = 0;
+            while(l1 != null || l2 != null)
+            {
+                int sum = carry;
+                if(l1 != null) { 
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if(l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                carry = sum / 10;
+                sum = sum % 10;
+
+                currentNode.val = sum;
+
+                if(carry != 0 && l1 == null && l2 == null)
+                {
+                    currentNode.next = new ListNode(carry);
+                    break;
+                }
+                if (carry == 0 && l1 == null && l2 == null)
+                {
+                    break;
+                }
+                currentNode.next = new ListNode();
+                currentNode = currentNode.next;
+            }
+            return parentNode;
+        }
+    }
+}
---
+++ LeetCodeTest/MediumProblems/AddTwoNumbersProblem.Test.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.MediumProblems;
+using LeetCode.Models;
+
+namespace LeetCodeTest.MediumProblems
+{
+    [TestClass]
+    public class AddTwoNumbersProblemTest
+    {
+        AddTwoNumbersProblem addTwoNumberProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            addTwoNumberProblem = new AddTwoNumbersProblem();
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersCase1()
+        {
+            int[] firstNodeNum = new int[] { 2, 4, 3 };
+            int[] secondNodeNum = new int[] { 5, 6, 4 };
+            int[] expectedNodeNum = new int[] { 7, 0, 8 };
+
+            ListNode firstNode = GenerateNode(firstNodeNum);
+            ListNode secondNode = GenerateNode(secondNodeNum);
+            ListNode expected = GenerateNode(expectedNodeNum);
+
+            ListNode actual = addTwoNumberProblem.AddTwoNumbers(firstNode, secondNode);
+            
+            Assert.IsTrue(NodesEqual(expected, actual));
+
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersCase2()
+        {
+            int[] firstNodeNum = new int[] { 9, 9, 9, 9, 9, 9, 9 };
+            int[] secondNodeNum = new int[] { 9, 9, 9, 9 };
+            int[] expectedNodeNum = new int[] { 8, 9, 9, 9, 0, 0, 0, 1 };
+
+            ListNode firstNode = GenerateNode(firstNodeNum);
+            ListNode secondNode = GenerateNode(secondNodeNum);
+            ListNode expected = GenerateNode(expectedNodeNum);
+
+            ListNode actual = addTwoNumberProblem.AddTwoNumbers(firstNode, secondNode);
+
+            Assert.IsTrue(NodesEqual(expected, actual));
+
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers2Case1()
+        {
+            int[] firstNodeNum = new int[] { 2, 4, 3 };
+            int[] secondNodeNum = new int[] { 5, 6, 4 };
+            int[] expectedNodeNum = new int[] { 7, 0, 8 };
+
+            ListNode firstNode = GenerateNode(firstNodeNum);
+            ListNode secondNode = GenerateNode(secondNodeNum);
+            ListNode expected = GenerateNode(expectedNodeNum);
+
+            ListNode actual = addTwoNumberProblem.AddTwoNumbers2(firstNode, secondNode);
+
+            Assert.IsTrue(NodesEqual(expected, actual));
+
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers2Case2()
+        {
+            int[] firstNodeNum = new int[] { 9, 9, 9, 9, 9, 9, 9 };
+            int[] secondNodeNum = new int[] { 9, 9, 9, 9 };
+            int[] expectedNodeNum = new int[] { 8, 9, 9, 9, 0, 0, 0, 1 };
+
+            ListNode firstNode = GenerateNode(firstNodeNum);
+            ListNode secondNode = GenerateNode(secondNodeNum);
+            ListNode expected = GenerateNode(expectedNodeNum);
+
+            ListNode actual = addTwoNumberProblem.AddTwoNumbers2(firstNode, secondNode);
+
+            Assert.IsTrue(NodesEqual(expected, actual));
+
+        }
+
+        public bool NodesEqual(ListNode n1,  ListNode n2)
+        {
+            while (n1 != null || n2 != null)
+            {
+                if (n1?.val != n2?.val) return false;
+                n1 = n1.next;
+                n2 = n2.next;
+            }
+            return true;
+        }
+
+        public ListNode GenerateNode(int[] numbers)
+        {
+            ListNode parentNode = new ListNode();
+            ListNode node = parentNode;
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                if (x == numbers.Length - 1)
+                {
+                    node.val = numbers[x];
+                    break;
+                }
+                node.val = numbers[x];
+                node.next = new ListNode();
+                node = node.next;
+            }
+           
+            return parentNode;
+        }
+    }
+}
