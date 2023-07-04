bcd6330cefa3cc02c03284e56f9598b38bb51f28
codieorberson <codieorberson@gmail.com>
2023-07-03 21:51:34 -0500

    Add backspacestringcompare question

---
+++ LeetCode/EasyProblems/BackspaceStringCompareProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class BackspaceStringCompareProblem
+    {
+        public BackspaceStringCompareProblem() { }
+
+        public bool BackspaceStringCompare(string string1, string string2)
+        {
+            Stack<char> firstStack = GenerateStack(string1);
+            Stack<char> secondStack = GenerateStack(string2);
+            return firstStack.SequenceEqual(secondStack);
+        }
+
+        public Stack<char> GenerateStack(string characters)
+        {
+            Stack<char> stack = new();
+            foreach (char c in characters)
+            {
+                if (c != '#') stack.Push(c);
+                else if(stack.Count != 0) stack.Pop();
+            }
+            return stack;
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/BackspaceStringCompareProblem.Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.EasyProblems;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class BackspaceStringCompareProblemTest
+    {
+        BackspaceStringCompareProblem backspaceStringCompareProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            backspaceStringCompareProblem = new BackspaceStringCompareProblem();
+        }
+
+        [TestMethod]
+        [DataRow("ab#c", "ad#c", true)]
+        [DataRow("ab##", "c#d#", true)]
+        [DataRow("a#c", "b", false)]
+        [DataRow("a##c", "#a#c", true)]
+        public void BackspaceStringCompare(string string1, string string2, bool expected)
+        {
+            bool actual = backspaceStringCompareProblem.BackspaceStringCompare(string1, string2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
