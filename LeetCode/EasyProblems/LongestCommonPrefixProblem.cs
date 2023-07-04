3db37dabedf824321da0635c087a8c3dcfb94f65
codieorberson <codieorberson@gmail.com>
2023-07-03 19:39:55 -0500

    Add Easy Problem

---
+++ LeetCode/EasyProblems/LongestCommonPrefixProblem.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class LongestCommonPrefixProblem
+    {
+        public LongestCommonPrefixProblem() { }
+
+        public string LongestCommonPrefix(string[] words)
+        {
+            
+            words = words.OrderBy(x => x.Length).ToArray();
+
+            int maxCount = words.ElementAt(0).Length;
+            string comparisonWord = words.ElementAt(0);
+           
+
+            string value = string.Empty;
+            for (int i = 0; i < maxCount; i++)
+            {
+                for (int j = 1; j < words.Length; j++)
+                {
+                    if (comparisonWord[i] != words[j][i])
+                    {
+                        return value;
+                    }
+                }
+                value += comparisonWord[i].ToString();
+
+            }
+
+            return value;
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/LongestCommonPrefixProblem.Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.EasyProblems;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class LongestCommonPrefixProblemTest
+    {
+
+        LongestCommonPrefixProblem longestCommonPrefixProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            longestCommonPrefixProblem = new LongestCommonPrefixProblem();
+        }
+
+        [TestMethod]
+        [DataRow(new string[] { "flower", "flow", "flight" }, "fl")]
+        [DataRow(new string[] { "dog", "racecar", "car" }, "")]
+        public void LongestCommonPrefix(string[] words, string expected)
+        {
+            string actual = longestCommonPrefixProblem.LongestCommonPrefix(words);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
