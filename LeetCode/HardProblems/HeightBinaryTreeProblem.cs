effa53800f7eb74d873d53ba1ba7321fd4942097
codieorberson <codieorberson@gmail.com>
2023-07-05 03:30:37 -0500

    Add ref logic

--- LeetCode/HardProblems/HeightBinaryTreeProblem.cs
+++ LeetCode/HardProblems/HeightBinaryTreeProblem.cs
@@ -4,39 +4,46 @@ namespace LeetCode.HardProblems
 {
     public class HeightBinaryTreeProblem
     {
-        public HeightBinaryTreeProblem() { }
+        public HeightBinaryTreeProblem()
+        {
+        }
 
         public int[] GetHeightWithSubtreeRemoval(TreeNode node, int[] queries)
         {
             List<int> heights = new();
-            foreach(int i in queries)
+            foreach (int i in queries)
             {
-                TreeNode newNode = RemoveNode(node, i);
-                heights.Add(GetHeight(newNode) -1);
+                TreeNode newRoot = CloneTree(node);
+                RemoveNode(ref newRoot, i);
+                heights.Add(GetHeight(ref newRoot));
             }
-
             return heights.ToArray();
         }
 
-        private TreeNode RemoveNode(TreeNode node, int value)
+        private void RemoveNode(ref TreeNode node, int value)
         {
-            if (node == null || node.val == value) return null;
-
-            TreeNode newNode = new TreeNode(node.val);
-            newNode.left = RemoveNode(node.left, value);
-            newNode.right = RemoveNode(node.right, value);
-
-            return newNode;
+            if (node == null) return;
+            if (node.val == value)
+            {
+                node = null; // This will remove the node, but will not reattach the children if there are any
+            }
+            else
+            {
+                RemoveNode(ref node.left, value);
+                RemoveNode(ref node.right, value);
+            }
         }
 
-        private int GetHeight(TreeNode node)
+        private int GetHeight(ref TreeNode node)
         {
-            if (node == null) return 0;
-
-            int leftTree = GetHeight(node.left);
-            int rightTree = GetHeight(node.right);
-
-            return Math.Max(leftTree, rightTree) + 1;
+            if (node == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1 + Math.Max(GetHeight(ref node.left), GetHeight(ref node.right));
+            }
         }
 
         public TreeNode CloneTree(TreeNode node)
@@ -45,7 +52,6 @@ namespace LeetCode.HardProblems
             {
                 return null;
             }
-
             TreeNode newNode = new TreeNode(node.val);
             newNode.left = CloneTree(node.left);
             newNode.right = CloneTree(node.right);
