229ecbbe898ef542d8eb36c1511e41cbb7319c44
codieorberson <codieorberson@gmail.com>
2023-07-05 15:21:26 -0500

    Add inclusive node range problem (no tests)

---
+++ LeetCode/EasyProblems/NodeInclusiveRangeProblem.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode.EasyProblems
+{
+    public class NodeInclusiveRangeProblem
+    {
+        public NodeInclusiveRangeProblem() { }
+
+        public int InclusiveRange(TreeNode root, int low, int high)
+        {
+            List<int> nodeValues = new List<int>();
+            GetNodeValues(root, nodeValues);
+            return(nodeValues.Where(x=> x >= low).Where(x=> x<=high).Sum());
+            
+        }
+
+        public void GetNodeValues(TreeNode root, List<int> nodeValues)
+        {
+            if (root == null) return;
+            nodeValues.Add(root.val);
+            if(root.left != null)
+            {
+                GetNodeValues(root.left, nodeValues);
+            }
+            if (root.right != null)
+            {
+                GetNodeValues(root.right, nodeValues);
+            }
+        }
+
+    }
+}
