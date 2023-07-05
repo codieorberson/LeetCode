640d93c935f8968135db8003729c461aedf5a6d1
codieorberson <codieorberson@gmail.com>
2023-07-04 21:29:55 -0500

    WIP

---
+++ LeetCode/MediumProblems/CatchBusProblem.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.MediumProblems
+{
+    public class CatchBusProblem
+    {
+        public CatchBusProblem() { }
+
+        public class BusPassenger
+        {
+            public int bus;
+            public int capacity;
+            public List<int> passengers;
+        }
+
+        public int LatestTimeCatchTheBus(int[] buses, int[] allPassengers, int capacity)
+        {
+            Array.Sort(buses);
+            Array.Sort(allPassengers);
+
+            List<int> passengers = allPassengers.ToList();
+            List<BusPassenger> busPassengers = new List<BusPassenger>();
+            for(int i = 0; i< buses.Length; i++)
+            {
+                List<int> currentPassenegers = passengers.Where(x => x <= buses[i]).OrderBy(x=>x).Take(capacity).ToList();
+                passengers = passengers.Where(x => !currentPassenegers.Contains(x)).ToList();
+
+                busPassengers.Add(new BusPassenger()
+                {
+                    bus = buses[i],
+                    capacity = capacity,
+                    passengers = currentPassenegers
+                });
+            }
+
+            int answer = DeclareLatestTime(busPassengers);
+            return answer;
+        }
+
+        public int DeclareLatestTime(List<BusPassenger> busPassengers)
+        {
+            BusPassenger lastBus = busPassengers.Last();
+
+            bool maxCapacityMet = lastBus.passengers.Count == lastBus.capacity;
+
+            int busCaughtTime = -1;
+            if (maxCapacityMet)
+            {
+                
+                foreach(int passenger in lastBus.passengers.OrderByDescending(x => x).ToList())
+                {
+                    if (busCaughtTime == -1) busCaughtTime = passenger - 1;
+                    else if (busCaughtTime == passenger) busCaughtTime--;
+                    else return busCaughtTime;
+                }
+            }
+            else
+            {
+                busCaughtTime = lastBus.bus;
+                foreach(int passenger in lastBus.passengers.OrderByDescending(x => x).ToList())
+                {
+                    if (passenger != busCaughtTime) return busCaughtTime;
+                    else busCaughtTime--;
+                }
+            }
+            return busCaughtTime;
+        }
+    }
+}
---
+++ LeetCodeTest/MediumProblems/CatchBusProblem.Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.MediumProblems;
+
+namespace LeetCodeTest.MediumProblems
+{
+    [TestClass]
+    public class CatchBusProblemTest
+    {
+        CatchBusProblem catchBusProblem;
+        [TestInitialize]
+        public void Setup()
+        {
+            catchBusProblem = new CatchBusProblem();
+        }
+
+        [DataRow(new int[] { 3 }, new int[] { 2,4 }, 2, 3)]
+        [DataRow(new int[] { 10, 20, 30 }, new int[] { 4, 11, 13, 19, 21, 25, 26 }, 2, 20)]
+        [DataRow(new int[] { 20, 30, 10 }, new int[] { 19, 13, 26, 4, 25, 11, 21 }, 2, 20)]
+        [DataRow(new int[] { 10, 20 }, new int[] { 2, 17, 18, 19 }, 2, 16)]
+        [DataRow(new int[] { 18, 8, 3, 12, 9, 2, 7, 13, 20, 5 }, new int[] { 13, 10, 8, 4, 12, 14, 18, 19, 5, 2, 30, 34 }, 1, 11)]
+        [TestMethod]
+        public void LatestTimeCatchTheBus(int[] buses, int[] passengers, int capacity, int expected)
+        {
+            int actual = catchBusProblem.LatestTimeCatchTheBus(buses, passengers, capacity);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
