5a17eb818e4c66609c54935ade67c42d3f909d81
codieorberson <codieorberson@gmail.com>
2023-07-05 00:48:16 -0500

    Add Create Node Forest Medium Problem

---
+++ LeetCode/MediumProblems/DeleteNodesReturnForestProblem.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+using LeetCode.Models;
+
+namespace LeetCode.MediumProblems
+{
+    public class DeleteNodesReturnForestProblem
+    {
+        IList<TreeNode> nodes;
+        public DeleteNodesReturnForestProblem()
+        {
+            nodes = new List<TreeNode>();
+        }
+
+        public IList<TreeNode> IsNodesForest(TreeNode root, int[] to_delete)
+        {
+
+            if(root != null && !to_delete.Contains(root.val))
+                nodes.Add(root);
+            CreateDFSNode(root, to_delete);
+
+            return nodes;
+        }
+
+        private TreeNode CreateDFSNode(TreeNode node, int[] to_delete)
+        {
+            if (node == null) return null;
+
+            node.left = CreateDFSNode(node.left, to_delete);
+            node.right = CreateDFSNode(node.right, to_delete);
+
+            if (to_delete.Contains(node.val))
+            {
+                if(node.left != null)
+                {
+                    nodes.Add(node.left);
+                }
+                if (node.right != null)
+                {
+                    nodes.Add(node.right);
+                }
+                return null;
+            }
+            return node;
+        }
+    }
+}
---
+++ LeetCodeTest/MediumProblems/DeleteNodesReturnForestProblem.Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.MediumProblems;
+using LeetCode.Models;
+
+namespace LeetCodeTest.MediumProblems
+{
+    [TestClass]
+    public class DeleteNodesReturnForestProblemTest
+    {
+        DeleteNodesReturnForestProblem deleteNodesReturnForestProblem;
+        [TestInitialize]
+        public void Setup() {
+            deleteNodesReturnForestProblem = new DeleteNodesReturnForestProblem();
+        }
+
+        [TestMethod]
+        [DataRow(new int[] {1,2,3,4,5,6,7}, new int[] { 3,5 })]
+        public void IsNodesForest(int[] rootNodes, int[] toDeleteNodes)
+        {
+            TreeNode? treeNode = GenerateTreeNode(rootNodes);
+            var actual = deleteNodesReturnForestProblem.IsNodesForest(treeNode, toDeleteNodes);
+        }
+
+        private TreeNode? GenerateTreeNode(int[] rootNodes)
+        {
+            return CreateBinaryTree(rootNodes, 0);
+        }
+
+        private TreeNode? CreateBinaryTree(int[] nodes, int index)
+        {
+            if (index >= nodes.Length || nodes[index] == 0)
+            {
+                return null;
+            }
+
+            TreeNode node = new TreeNode(nodes[index]);
+            node.left = CreateBinaryTree(nodes, 2 * index + 1);
+            node.right = CreateBinaryTree(nodes, 2 * index + 2);
+
+            return node;
+        }
+    }
+}
