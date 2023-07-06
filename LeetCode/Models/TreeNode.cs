3058d54dfc4f11753fe08d288ca756194c28a5b9
codieorberson <codieorberson@gmail.com>
2023-07-05 19:45:21 -0500

    Add helper functionm to treenode

--- LeetCode/Models/TreeNode.cs
+++ LeetCode/Models/TreeNode.cs
@@ -24,5 +24,16 @@
 
             return node;
         }
+
+        public bool AreEqual(TreeNode node1, TreeNode node2)
+        {
+            if (node1 == null && node2 == null) return true;
+
+            if (node1 == null || node2 == null) return false;
+
+            if (node1.val != node2.val) return false;
+
+            return AreEqual(node1.right, node2.right) && AreEqual(node1.left, node2.left);
+        }
     }
 }
