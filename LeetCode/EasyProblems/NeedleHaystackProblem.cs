7a7c46cf761cb92f8b9bc37afc784accf4e4da74
codieorberson <codieorberson@gmail.com>
2024-04-30 00:30:12 -0500

    WIP

---
+++ LeetCode/EasyProblems/NeedleHaystackProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Services
+{
+    public class NeedleHaystackProblem
+    {
+
+        public NeedleHaystackProblem() { }
+
+        public int NeedleHaystack(string needle, string haystack)
+        {
+            if (!haystack.Contains(needle))
+            {
+                return -1;
+            }
+
+            int solutionIndex = 0;
+            int needleIndex = 0;
+            int needleLength = needle.Length -1;
+            for(int i = 0; i< haystack.Length; i++ )
+            {
+                if (haystack[i] == needle[needleIndex] && needleLength == needleIndex)
+                {
+                    return solutionIndex;
+                }
+                else if (haystack[i] == needle[needleIndex])
+                {
+                    if (needleIndex == 0)
+                    {
+                        solutionIndex = i;
+                    }
+                    needleIndex++;
+                }
+                else if (haystack[i] != needle[needleIndex])
+                {
+                    needleIndex = 0;
+                    solutionIndex = i + 1;
+                }
+            }
+            return solutionIndex;
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/NeedleHaystackProblem.Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Services;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class NeedleHaystackProblemTest
+    {
+        NeedleHaystackProblem NeedleHaystackProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            NeedleHaystackProblem = new NeedleHaystackProblem();
+        }
+
+        [TestMethod]
+        [DataRow("sad", "sadbutsad", 0)]
+        [DataRow("sad", "bsadbutsad", 1)]
+        [DataRow("leeto", "leetcode", -1)]
+        [DataRow("c", "abc", 2)]
+        [DataRow("issip", "mississippi", 4)]
+        public void TwoSum(string needle, string haystack, int expected)
+        {
+            var result = NeedleHaystackProblem.NeedleHaystack(needle, haystack);
+
+            Assert.AreEqual(result, expected);        
+        }
+
+
+    }
+}
