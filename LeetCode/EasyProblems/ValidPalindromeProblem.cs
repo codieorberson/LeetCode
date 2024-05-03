14ab7ed9eeb9333a0b2cb1e2fac38738cf1e7b7f
codieorberson <codieorberson@gmail.com>
2024-05-02 19:50:44 -0500

    Add Valid Palindrome problem

---
+++ LeetCode/EasyProblems/ValidPalindromeProblem.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class ValidPalindromeProblem
+    {
+        public ValidPalindromeProblem() { }
+
+        public bool ValidPalindrome(string phrase)
+        {
+
+            phrase = phrase.ToLower();
+            string filteredChars = "!@#$%^&*()-_=+[{]};:'\",<.>/? `~\\|";
+            
+            foreach(char filteredChar in  filteredChars)
+            {
+                phrase = phrase.Replace(filteredChar.ToString(), "");
+            }
+            string reversedPhrase = new string(phrase.Reverse().ToArray());
+            return reversedPhrase == phrase;
+        }
+
+        public bool ValidPalindrome2(string phrase)
+        {
+            phrase = phrase.ToLower();
+            string filteredPhrase = "";
+            foreach (char filteredChar in phrase)
+            {
+                if (char.IsLetterOrDigit(filteredChar))
+                {
+                    filteredPhrase += filteredChar;
+                }
+            }
+            string reversedPhrase = new string(filteredPhrase.Reverse().ToArray());
+            return reversedPhrase == filteredPhrase;
+        }
+
+    }
+}
---
+++ LeetCodeTest/EasyProblems/ValidPalindromeProblem.Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.EasyProblems;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+    public class ValidPalindromeProblemTest
+    {
+
+        ValidPalindromeProblem validPalindromeProblem;
+        [TestInitialize]
+        public void Setup()
+        {
+            validPalindromeProblem = new ValidPalindromeProblem();
+        }
+
+        [TestMethod]
+        [DataRow("A man, a plan, a canal: Panama", true)]
+        [DataRow("race a car", false)]
+        [DataRow("Marge, let's \"[went].\" I await {news} telegram.", true)]
+        public void ValidPalindrome(string phrase, bool expected)
+        {
+
+            bool actual = validPalindromeProblem.ValidPalindrome(phrase);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
