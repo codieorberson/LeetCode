c04bfbb5fd10b7794f0f80bff595415535bc1f0e
codieorberson <codieorberson@gmail.com>
2024-05-04 12:12:07 -0500

    Add Sqrt Problem

---
+++ LeetCode/EasyProblems/SqrtProblem.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class SqrtProblem
+    {
+        public SqrtProblem() { }
+
+        //Slow
+        public int GetSqrtForInt(int number)
+        {
+            for(int i =1; i <= int.MaxValue; i++)
+            {
+                int possibleValue = i * i;
+                if(possibleValue == number)
+                {
+                    return i;
+                }
+                else if(possibleValue > number)
+                {
+                    return i - 1;
+                }
+            }
+            return -1;
+        }
+
+        public double GetSqrtForInt2(int number)
+        {
+            return Math.Floor(Math.Sqrt(number));
+        }
+
+        public int GetSqrtForInt3(int number)
+        {
+            if (number == 0 || number == 1)
+                return number; 
+
+            long start = 1, end = number / 2, ans = 0;
+            while (start <= end)
+            {
+                long mid = (start + end) / 2;
+                long square = mid * mid;
+
+                if (square == number)
+                    return (int)mid;
+                if (square < number)
+                {
+                    start = mid + 1;
+                    ans = mid;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return (int)ans;
+        }
+    }
+}
---
+++ LeetCodeTest/EasyProblems/SqrtProblem.Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.EasyProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest.EasyProblems
+{
+    [TestClass]
+
+    public class SqrtProblemTest
+    {
+        SqrtProblem sqrtProblem;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sqrtProblem = new SqrtProblem();
+        }
+
+        [TestMethod]
+        [DataRow(2147483647, 46340)]
+        [DataRow(65, 8)]
+        [DataRow(8, 2)]
+        public void GetSqrtForInt(int number, int expected)
+        {
+            var actual = sqrtProblem.GetSqrtForInt(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
